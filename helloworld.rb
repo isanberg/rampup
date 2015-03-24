051cd3557ff347a838daba7780581666bbaa1c49
isanberg <ian.isanberg@gmail.com>
2015-03-23 21:42:50 -0400

    moved line

--- helloworld.rb
+++ helloworld.rb
@@ -6,6 +6,7 @@ current_temperature = gets.strip.to_f
 puts "Is this in celsius or Fahrenheit? [C/F]"
 original_temperature = gets.strip[0].downcase
 
+
 if original_temperature == 'c'
     new_temperature = (current_temperature * 5 / 9) + 32
     puts "New temperature: #{new_temperature} F"
